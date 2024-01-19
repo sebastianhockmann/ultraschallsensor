645347ceed2a848b58497a0ccb2cff0e3890f97a
sebastianhockmann <156853550+sebastianhockmann@users.noreply.github.com>
2024-01-19 17:38:10 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
+let sensor = 0
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    led.plotBarGraph(
-    pins.pulseIn(DigitalPin.P1, PulseValue.High),
-    0
-    )
-    basic.pause(100)
+    sensor += grove.measureInCentimeters(DigitalPin.C16)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(sensor)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+    }
 })
